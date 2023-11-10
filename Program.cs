760da6b0afc2259d8a365de95da93e44af017482
nikolas nunes <nunesnikolas100@gmail.com>
2023-11-09 23:27:34 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // digitar a tabuada desejada
+             
+            Console.WriteLine("digite o numero da tabuada?");
+
+            int numero =int.Parse(Console.ReadLine());
+
+            //mostrar o resultado
+            Console.WriteLine("resposta");
+            
+            for (int i = 0; i <= 10; i++)
+                
+
+            {
+                int total = numero * i;
+                Console.WriteLine(numero + "x" + i + "=" + total);
+                
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
